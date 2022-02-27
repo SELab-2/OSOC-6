a02f3b1150ff753d9d89c291c1443340e23ec90f
Ruben Van Mello <Ruben.VanMello@ugent.be>
2022-02-27 14:22:27 +0100

    Config spring and database

---
+++ src/main/java/com/osoc6/OSOC6/Osoc6Application.java
@@ -0,0 +1,13 @@
+package com.osoc6.OSOC6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Osoc6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Osoc6Application.class, args);
+	}
+
+}
