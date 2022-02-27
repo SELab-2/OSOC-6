c7ae97febc13d7316fa635541b59085494865181
Ruben Van Mello <Ruben.VanMello@ugent.be>
2022-02-27 19:09:12 +0100

    Java 17 Spring and database connection example

---
+++ src/test/java/com/osoc6/OSOC6/Osoc6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osoc6.OSOC6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Osoc6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
