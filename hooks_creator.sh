b18236e0ddbb3ea85345bd78130c85884eee20c6
Jitse De Smet <jitse@reedonk8.be>
2022-03-06 19:54:08 +0100

    correct hooks

--- hooks_creator.sh
+++ hooks_creator.sh
@@ -7,17 +7,17 @@ cd .git/hooks
 echo "#!/bin/bash" > pre-commit
 echo "cd backend/" >> pre-commit
 echo "./gradlew task check" >> pre-commit
-echo "RESULT=$?" >> pre-commit
+echo "RESULT=\$?" >> pre-commit
 echo "[ \$RESULT -ne 0 ] && exit 1" >> pre-commit
 echo "exit 0" >> pre-commit
 
 echo "cd ../frontend/" >> pre-commit
 echo "npm run lint" >> pre-commit
-echo "RESULT=$?" >> pre-commit
+echo "RESULT=\$?" >> pre-commit
 echo "[ \$RESULT -ne 0 ] && exit 1" >> pre-commit
 
 echo "npx prettier --check ." >> pre-commit
-echo "RESULT=$?" >> pre-commit
+echo "RESULT=\$?" >> pre-commit
 echo "[ \$RESULT -ne 0 ] && exit 1" >> pre-commit
 
 chmod u+x pre-commit
