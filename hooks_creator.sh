2675b2de8eea3a4c2f4418febd60225d4e38b291
Jitse De Smet <jitse@reedonk8.be>
2022-03-06 15:06:23 +0100

    hooks need to be set by the user. added a script to do that

---
+++ hooks_creator.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Set location to script location
+cd "$(dirname "$0")"
+
+cd .git/hooks
+
+echo "#!/bin/bash" > pre-commit
+echo "cd backend/" >> pre-commit
+echo "./gradlew task check" >> pre-commit
+echo "RESULT=$?" >> pre-commit
+echo "[ \$RESULT -ne 0 ] && exit 1" >> pre-commit
+echo "exit 0" >> pre-commit
