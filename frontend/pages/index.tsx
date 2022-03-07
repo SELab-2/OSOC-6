fafbc1d7ba98f7de3518d529ea55424b5ff10a67
Jitse De Smet <35114273+jitsedesmet@users.noreply.github.com>
2022-03-07 12:49:35 +0100

    Management/25 setup eslint + prettier (#33)

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,19 +3,17 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  return (
-      <div className={styles.container}>
-        <Head>
-          <title>OSOC-6</title>
-        </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>OSOC-6</title>
+            </Head>
 
-        <main className={styles.main}>
-          <h1 className={styles.title}>
-            Open Summer of Code Tool
-          </h1>
-        </main>
-      </div>
-  )
+            <main className={styles.main}>
+                <h1 className={styles.title}>Open Summer of Code Tool</h1>
+            </main>
+        </div>
+    )
 }
 
 export default Home
