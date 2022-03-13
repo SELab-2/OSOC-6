a204a51cecb55868902fbf40c79da196ae84459e
Jitse De Smet <jitse@reedonk8.be>
2022-03-13 13:19:52 +0100

    (broken) OneToMany and ManyToOne base correct implementation

--- backend/src/main/java/com/osoc6/OSOC6/database/models/student/Study.java
+++ backend/src/main/java/com/osoc6/OSOC6/database/models/student/Study.java
@@ -1,6 +1,9 @@
 package com.osoc6.OSOC6.database.models.student;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Study {
