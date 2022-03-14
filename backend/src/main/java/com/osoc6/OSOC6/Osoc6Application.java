d6f19ae56a0e3e183d944fed59bdae15ddd7011b
Kasper Demeyere <kasperdemeyere@hotmail.com>
2022-03-14 11:38:05 +0100

    added validation properties

--- backend/src/main/java/com/osoc6/OSOC6/Osoc6Application.java
+++ backend/src/main/java/com/osoc6/OSOC6/Osoc6Application.java
@@ -2,6 +2,10 @@ package com.osoc6.OSOC6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
 public class Osoc6Application {
@@ -12,6 +16,21 @@ public class Osoc6Application {
      */
     public static void main(final String[] args) {
         SpringApplication.run(Osoc6Application.class, args);
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:validationmessages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
     }
 
 }
