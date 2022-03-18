fe68daea23ae1e0f670cef074292e01d89a2be19
Jitse De Smet <jitse@reedonk8.be>
2022-03-18 14:21:19 +0100

    try first setup

---
+++ frontend/src/helper/removableHelper.ts
@@ -0,0 +1,8 @@
+/**
+ * takes the sum of 2 numbers. Only here to test the test suite.
+ * @param first
+ * @param second
+ */
+export function sum(first: number, second: number) {
+    return first + second;
+}
