aac848a65a40cae685f5c3411c28e46d06ace655
Jitse De Smet <jitse@reedonk8.be>
2022-03-22 20:42:56 +0100

    edit small mistake

--- backend/src/test/java/com/osoc6/OSOC6/AuthenticationTest.java
+++ backend/src/test/java/com/osoc6/OSOC6/AuthenticationTest.java
@@ -1,13 +1,12 @@
 package com.osoc6.OSOC6;
 
-import org.aspectj.lang.annotation.Before;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.FormLoginRequestBuilder;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -29,7 +28,7 @@ public class AuthenticationTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Before("")
+    @Before
     public void setUp() {
         mockMvc = MockMvcBuilders.webAppContextSetup(context)
                 .apply(springSecurity())
