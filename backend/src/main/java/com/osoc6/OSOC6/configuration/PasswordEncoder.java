b478b18748714fd53a1f225150da5efefa133647
Kasper Demeyere <68854162+KasperDemeyere@users.noreply.github.com>
2022-03-22 22:19:35 +0100

    Validation properties file and config to load it (#118)
    * add configuration to load properties file
    
    * added configuration folder
    
    * updated javadoc
    
    * Update backend/src/main/java/com/osoc6/OSOC6/configuration/WebConfiguration.java
    
    Co-authored-by: Thomas Van Mullem <99752211+thvmulle@users.noreply.github.com>
    
    Co-authored-by: Thomas Van Mullem <99752211+thvmulle@users.noreply.github.com>

--- backend/src/main/java/com/osoc6/OSOC6/configuration/PasswordEncoder.java
+++ backend/src/main/java/com/osoc6/OSOC6/configuration/PasswordEncoder.java
@@ -1,4 +1,4 @@
-package com.osoc6.OSOC6.security;
+package com.osoc6.OSOC6.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
---
+++ backend/src/main/java/com/osoc6/OSOC6/configuration/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This module contains the configuration related classes.
+ *
+ * @since 0.0
+ * @author andpuydt
+ * @version 0.0
+ */
+
+package com.osoc6.OSOC6.configuration;
