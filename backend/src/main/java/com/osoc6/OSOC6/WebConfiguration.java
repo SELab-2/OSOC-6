959c792a022c312eea2af1c20d8a70ffd2d4a80d
Kasper Demeyere <kasperdemeyere@hotmail.com>
2022-03-22 21:38:31 +0100

    add configuration to load properties file

---
+++ backend/src/main/java/com/osoc6/OSOC6/WebConfiguration.java
@@ -0,0 +1,37 @@
+package com.osoc6.OSOC6;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+/**
+ * The configuration of the application.
+ * This class wil load and set up anything non-default needed to run the application.
+ */
+@Configuration
+public class WebConfiguration {
+
+    /**
+     * Get the message source application.properties file.
+     * @return the message source
+     */
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:validation-messages");
+        return messageSource;
+    }
+
+    /**
+     * Set up the LocalValidatorFactoryBean to use messages from the specified message source.
+     * @return the configured LocalValidatorFactoryBean
+     */
+    @Bean
+    public LocalValidatorFactoryBean getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+}
