71cec671d937cd881cba7d0337869a1c3712c236
lvrossem <lukas.vanrossem1@gmail.com>
2022-03-23 13:17:31 +0100

    Add OrganisationRepository

---
+++ backend/src/main/java/com/osoc6/OSOC6/repository/OrganisationRepository.java
@@ -0,0 +1,24 @@
+package com.osoc6.OSOC6.repository;
+
+import com.osoc6.OSOC6.database.models.Organisation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+/**
+ * This is a simple class that defines a repository for Organisation,
+ * this is needed for the database.
+ *
+ * @author lvrossem
+ */
+@RepositoryRestResource(collectionResourceRel = "organisations", path = "organisations")
+public interface OrganisationRepository extends JpaRepository<Organisation, Long> {
+    /**
+     * Find an organisation by its name.
+     * @param name the searched name
+     * @return list of matched organisations
+     */
+    List<Organisation> findByName(@Param("name") String name);
+}
