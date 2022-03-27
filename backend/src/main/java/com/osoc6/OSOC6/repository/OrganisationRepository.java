c63b05db50ce59182bc463b39f333dc29cf4534e
lvrossem <lukas.vanrossem1@gmail.com>
2022-03-27 17:03:53 +0200

    Add OrganisationRepository and corresponding path

---
+++ backend/src/main/java/com/osoc6/OSOC6/repository/OrganisationRepository.java
@@ -0,0 +1,26 @@
+package com.osoc6.OSOC6.repository;
+
+import com.osoc6.OSOC6.database.models.Organisation;
+import com.osoc6.OSOC6.winterhold.DumbledorePathWizard;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+/**
+ * This is a simple class that defines a repository for Organisation.
+ * This creates default restful endpoints and allows us to access the database in a restful manner.
+ *
+ * @author lvrossem
+ */
+@RepositoryRestResource(collectionResourceRel = DumbledorePathWizard.ORGANISATIONS_PATH,
+        path = DumbledorePathWizard.ORGANISATIONS_PATH)
+public interface OrganisationRepository extends JpaRepository<Organisation, Long> {
+    /**
+     * Find an organisation by its name.
+     * @param name the name of the searched organisation
+     * @return list of matched organisations
+     */
+    List<Organisation> findByName(@Param("name") String name);
+}
