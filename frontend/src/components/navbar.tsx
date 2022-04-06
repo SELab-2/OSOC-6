fb7ecaecfd7f09a4786d323ae5b8ef4cc3cd48df
lvrossem <lukas.vanrossem1@gmail.com>
2022-04-06 23:23:17 +0200

    Remove hashtags in URLs

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,18 +1,26 @@
-import {Navbar, Nav, Container} from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 export const NavBar = () => {
     return (
         <>
-            <Navbar collapseOnSelect fixed='top' expand="sm" bg="dark" variant="dark">
+            <Navbar
+                collapseOnSelect
+                fixed="top"
+                expand="sm"
+                bg="dark"
+                variant="dark"
+            >
                 <Container>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link href="#students">Students</Nav.Link>
-                            <Nav.Link href="#users">Users</Nav.Link>
-                            <Nav.Link href="#projects">Projects</Nav.Link>
-                            <Nav.Link href="#assignStudents">Assign Students</Nav.Link>
-                            <Nav.Link href="#profile">Profile</Nav.Link>
+                            <Nav.Link href="students">Students</Nav.Link>
+                            <Nav.Link href="users">Users</Nav.Link>
+                            <Nav.Link href="projects">Projects</Nav.Link>
+                            <Nav.Link href="assignStudents">
+                                Assign Students
+                            </Nav.Link>
+                            <Nav.Link href="profile">Profile</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
