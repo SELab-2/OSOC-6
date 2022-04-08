5e9ba40ae83cf5a215a0cdb4a62e87ab24ff1b86
andpuydt <anne.depuydt@ugent.be>
2022-04-08 23:20:53 +0200

    Use translation on index page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import useTranslation from "next-translate/useTranslation";
 
 const BeginPage: NextPage = () => {
+    const { t } = useTranslation('translations');
     return (
         <div>
             <Head>
-                <title>Open Summer of Code</title>
+                <title>{t('Home page title')}</title>
             </Head>
             <main className="m-4">
                 <h1>Empty page</h1>
