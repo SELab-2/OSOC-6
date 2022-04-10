16d8b958dfa99d99c31ee956a92c38c2c7c73c8a
Jitse De Smet <jitse@reedonk8.be>
2022-04-10 18:52:02 +0200

    implement pages

--- frontend/src/api/Entities.ts
+++ frontend/src/api/Entities.ts
@@ -1,27 +1,73 @@
-export interface BaseEntity {
-    _links: { self: { href : string } } | undefined
+export interface Referencer {
+    href: string,
 }
 
+export interface BaseEntity {
+    _links: { self: Referencer } | undefined;
+}
+
+
+export interface Page<T> {
+    page: {
+        number: number,
+        size: number,
+        totalElements: number,
+        totalPages: number,
+    },
+    _links: {
+        self: Referencer
+        search: Referencer
+        profile: Referencer
+    },
+    _embedded: T
+}
+
+export type editionPage = Page<{editions: IEdition}>
+
+export type userPage = Page<{users: IUser}>
+
 export interface IUser extends BaseEntity {
-    accountNonExpired: boolean,
-    accountNonLocked: boolean,
-    authorities: Authority
-    callName: string,
-    credentialsNonExpired: boolean,
-    email: string,
-    enabled: boolean,
-    userRole: UserRole,
-    username: string,
+    accountNonExpired: boolean;
+    accountNonLocked: boolean;
+    authorities: Authority;
+    callName: string;
+    credentialsNonExpired: boolean;
+    email: string;
+    enabled: boolean;
+    userRole: UserRole;
+    username: string;
+}
+
+export interface IInvitation extends BaseEntity {
+    token: string | undefined,
+    creationTimestamp: Date | undefined,
+    edition: string,
+    issuer: string,
+    subject: string | undefined;
+}
+
+export class Invitation implements IInvitation {
+
+    constructor(issuer: string, edition: string) {
+        this.issuer = issuer;
+        this.edition = edition;
+    }
+
+    _links: { self: { href: string } } | undefined;
+    creationTimestamp: Date | undefined;
+    edition: string;
+    issuer: string;
+    subject: string | undefined;
+    token: string | undefined;
 }
 
 export interface IEdition extends BaseEntity {
-    name: string,
-    year: number,
-    active: boolean,
+    name: string;
+    year: number;
+    active: boolean;
 }
 
 export class Edition implements IEdition {
-
     constructor(name: string, year: number, active: boolean) {
         this.name = name;
         this.active = active;
@@ -35,10 +81,10 @@ export class Edition implements IEdition {
 }
 
 export enum UserRole {
-    admin = "ADMIN",
-    coach = "COACH",
+    admin = 'ADMIN',
+    coach = 'COACH',
 }
 
 export interface Authority {
-    authority: UserRole
+    authority: UserRole;
 }
--- frontend/src/handlers/dataInjectionHandler.ts
+++ frontend/src/handlers/dataInjectionHandler.ts
@@ -1,7 +1,7 @@
 import { MouseEventHandler } from 'react';
 import axios from 'axios';
-import pathNames from "../properties/pathNames";
-import { Edition, IUser } from "../api/Entities";
+import pathNames from '../properties/pathNames';
+import { Edition, Invitation, IUser } from "../api/Entities";
 
 const baseRef = { baseURL: pathNames.base };
 
@@ -9,15 +9,17 @@ export const dataInjectionHandler: MouseEventHandler<
     HTMLButtonElement
 > = async (_) => {
     const result = await axios.get(pathNames.ownUser, baseRef);
-    const user: IUser = <IUser> result.data;
+    const user: IUser = <IUser>result.data;
 
-    const own_user: string | undefined = user._links ? user._links.self.href : undefined;
+    const own_user: string | undefined = user._links
+        ? user._links.self.href
+        : undefined;
 
     const editions = (await axios.get(pathNames.editions, baseRef)).data;
 
     const invitation = (await axios.get(pathNames.invitations, baseRef)).data;
 
-    const edition1: Edition = new Edition("Edition 1", 2022, true);
+    const edition1: Edition = new Edition('Edition 1', 2022, true);
 
     //await axios.post(pathNames.editions, edition1, baseRef
 
