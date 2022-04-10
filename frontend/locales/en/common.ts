ce0e1d7ffe2182a6590837e628c9c147be0f1fdc
andpuydt <anne.depuydt@ugent.be>
2022-04-10 14:49:32 +0200

    Use correct login url

--- frontend/locales/en/common.ts
+++ frontend/locales/en/common.ts
@@ -1,13 +1,13 @@
 const common = {
-    "Tool name": "Open Summer Of Code Tool",
-    "Home page title": "OSOC-6",
-    "Login page title": "OSOC : Login page",
-    "Empty page": "Emtpy page",
-    "Navbar students": "Students",
-    "Navbar users": "Users",
-    "Navbar projects": "Projects",
-    "Navbar assign students": "Assign Students",
-    "Navbar profile": "Profile"
+    'Tool name': 'Open Summer Of Code Tool',
+    'Home page title': 'OSOC-6',
+    'Login page title': 'OSOC : Login page',
+    'Empty page': 'Emtpy page',
+    'Navbar students': 'Students',
+    'Navbar users': 'Users',
+    'Navbar projects': 'Projects',
+    'Navbar assign students': 'Assign Students',
+    'Navbar profile': 'Profile',
 };
 
 export default common;
