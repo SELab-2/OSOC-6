ac4395f6f9243db45f4df86b53a1fadafee71a6b
Jitse De Smet <35114273+jitsedesmet@users.noreply.github.com>
2022-04-12 10:36:59 +0200

    Update frontend/src/handlers/dataInjectionHandler.ts
    Co-authored-by: Kasper Demeyere <68854162+KasperDemeyere@users.noreply.github.com>

--- frontend/src/handlers/dataInjectionHandler.ts
+++ frontend/src/handlers/dataInjectionHandler.ts
@@ -297,7 +297,7 @@ export const dataInjectionHandler: MouseEventHandler<HTMLButtonElement> = async 
                     ).data
             )
         );
-        console.log('You registered a new user. You are now logged out.');
+        console.log('You registered a new user. You are now logged out. This happens due to a bug in the backend. It will be fixed eventually.');
     } else {
         let containedUsers = (<IUsersPage>(await axios.get(pathNames.users, AxiosConf)).data)
             ._embedded.users;
