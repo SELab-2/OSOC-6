38c9a9291b4d0c5a753daa2afac6c40e161d5276
Jitse De Smet <jitse@reedonk8.be>
2022-04-12 14:30:14 +0200

    add docs

--- backend/src/test/java/com/osoc6/OSOC6/TestEntityProvider.java
+++ backend/src/test/java/com/osoc6/OSOC6/TestEntityProvider.java
@@ -17,11 +17,17 @@ import com.osoc6.OSOC6.database.models.student.Student;
 import java.util.List;
 
 /**
- * This class provides some entities to classes it doesn't need any web so it can be a Utility class.
+ * This class provides some entities to classes it doesn't need any web calls so it can be a Utility class.
+ * Using this class you can create base entities more easily.
  */
 public final class TestEntityProvider {
     private TestEntityProvider() { }
 
+    /**
+     * Creates a student with pronouns HE.
+     * @param performer the {@link BaseTestPerformer} this entity is created for
+     * @return the requested entity
+     */
     public static Student getBaseStudentHe(final BaseTestPerformer<?, ?, ?> performer) {
         return Student.builder()
                 .email("kasper@mail.com")
@@ -50,6 +56,11 @@ public final class TestEntityProvider {
                 .build();
     }
 
+    /**
+     * Creates a student with pronouns Other.
+     * @param performer the {@link BaseTestPerformer} this entity is created for
+     * @return the requested entity
+     */
     public static Student getBaseStudentOther(final BaseTestPerformer<?, ?, ?> performer) {
         return Student.builder()
                 .email("jitse@mail.com")
@@ -80,61 +91,131 @@ public final class TestEntityProvider {
                 .build();
     }
 
+    /**
+     * Creates a base project.
+     * @param performer the {@link BaseTestPerformer} this entity is created for
+     * @return the requested entity
+     */
     public static Project getBaseProject1(final BaseTestPerformer<?, ?, ?> performer) {
         return new Project("New chip", performer.getBaseUserEdition(), "Intel", performer.getAdminUser());
     }
 
+    /**
+     * Creates a base project.
+     * @param performer the {@link BaseTestPerformer} this entity is created for
+     * @return the requested entity
+     */
     public static Project getBaseProject2(final BaseTestPerformer<?, ?, ?> performer) {
         return new Project("Instagram", performer.getBaseUserEdition(), "Meta", performer.getAdminUser());
     }
 
+    /**
+     * Creates a base communication template.
+     * @param performer the {@link BaseTestPerformer} this entity is created for
+     * @return the requested entity
+     */
     public static CommunicationTemplate getBaseCommunicationTemplate1(final BaseTestPerformer<?, ?, ?> performer) {
         return new CommunicationTemplate("A well deserved yes",
                 "We would like to inform you... You are the best candidate we ever had! We want you! Need you!");
     }
 
+    /**
+     * Creates a base communication template.
+     * @param performer the {@link BaseTestPerformer} this entity is created for
+     * @return the requested entity
+     */
     public static CommunicationTemplate getBaseCommunicationTemplate2(final BaseTestPerformer<?, ?, ?> performer) {
         return new CommunicationTemplate("Love Letter",
                 "Romeo Oh romeo thou make my hearth melt.");
     }
 
+    /**
+     * Creates a non active edition.
+     * @param performer the {@link BaseTestPerformer} this entity is created for
+     * @return the requested entity
+     */
     public static Edition getBaseNonActiveEdition(final BaseTestPerformer<?, ?, ?> performer) {
         return new Edition("Edition 1", 0, false);
     }
 
+    /**
+     * Creates an active edition.
+     * @param performer the {@link BaseTestPerformer} this entity is created for
+     * @return the requested entity
+     */
     public static Edition getBaseActiveEdition(final BaseTestPerformer<?, ?, ?> performer) {
         return new Edition("Edition 2", 1, true);
     }
 
+    /**
+     * Creates a non-used invitation.
+     * @param performer the {@link BaseTestPerformer} this entity is created for
+     * @return the requested entity
+     */
     public static Invitation getBaseUnusedInvitation(final BaseTestPerformer<?, ?, ?> performer) {
         return new Invitation(performer.getBaseUserEdition(), performer.getAdminUser(), null);
     }
 
+    /**
+     * Creates a basic skilltype.
+     * @param performer the {@link BaseTestPerformer} this entity is created for
+     * @return the requested entity
+     */
     public static SkillType getBaseSkillType1(final BaseTestPerformer<?, ?, ?> performer) {
         return new SkillType("Skilltype 1", "42B37B");
     }
 
+    /**
+     * Creates a basic skilltype.
+     * @param performer the {@link BaseTestPerformer} this entity is created for
+     * @return the requested entity
+     */
     public static SkillType getBaseSkillType2(final BaseTestPerformer<?, ?, ?> performer) {
         return new SkillType("Skilltype 2", "C94040");
     }
 
+    /**
+     * Creates a base UserEntity that has ADMIN role.
+     * @param performer the {@link BaseTestPerformer} this entity is created for
+     * @return the requested entity
+     */
     public static UserEntity getBaseAdminUserEntity(final BaseTestPerformer<?, ?, ?> performer) {
         return new UserEntity("test.admin@test.com", "Tester The Test", UserRole.ADMIN, "123456");
     }
 
+    /**
+     * Creates a base UserEntity that has COACH role.
+     * @param performer the {@link BaseTestPerformer} this entity is created for
+     * @return the requested entity
+     */
     public static UserEntity getBaseCoachUserEntity(final BaseTestPerformer<?, ?, ?> performer) {
         return new UserEntity("test.coach@test.com", "Tester The Test", UserRole.COACH, "123456");
     }
 
+    /**
+     * Creates a UserSkill for the admin user.
+     * @param performer the {@link BaseTestPerformer} this entity is created for
+     * @return the requested entity
+     */
     public static UserSkill getBaseAdminUserSkill(final BaseTestPerformer<?, ?, ?> performer) {
         return new UserSkill("Active like Duracel bunny", performer.getAdminUser(), "The admin is very active");
     }
 
+    /**
+     * Creates a UserSkill for the coach user.
+     * @param performer the {@link BaseTestPerformer} this entity is created for
+     * @return the requested entity
+     */
     public static UserSkill getBaseCoachUserSkill(final BaseTestPerformer<?, ?, ?> performer) {
         return new UserSkill("Coaching with passion", performer.getCoachUser(),
                 "I just want my students to learn.");
     }
 
+    /**
+     * Creates a UserSkill for the outsider user.
+     * @param performer the {@link BaseTestPerformer} this entity is created for
+     * @return the requested entity
+     */
     public static UserSkill getBaseOutsiderUserSkill(final BaseTestPerformer<?, ?, ?> performer) {
         return new UserSkill("Lonely loner", performer.getOutsiderCoach(),
                 "Stands outside in the rain and cries.");
