95560e7144679ff9f0e92e088406890e94f5a13e
thvmulle <Thomas.VanMullem@UGent.be>
2022-04-13 12:16:49 +0200

    Save accordion, no crlf (fr this time)

--- frontend/src/components/projectAssignmentList.tsx
+++ frontend/src/components/projectAssignmentList.tsx
@@ -1,49 +1,53 @@
-import {NextPage} from "next";
-import {Accordion} from "react-bootstrap";
+import { NextPage } from "next";
+import { Accordion } from "react-bootstrap";
 import AccordionHeader from "react-bootstrap/AccordionHeader";
 import AccordionBody from "react-bootstrap/AccordionBody";
 import AccordionItem from "react-bootstrap/AccordionItem";
 import axios from "axios";
 import pathNames from "../properties/pathNames";
-import {useEffect, useState} from "react";
-import {AxiosConf} from "../api/requests";
-import {IProject, IProjectPage} from "../api/ProjectEntity";
-import {IAssignmentPage} from "../api/AssignmentEntity";
-
+import { useEffect, useState } from "react";
+import { AxiosConf } from "../api/requests";
+import { IProject, IProjectPage } from "../api/ProjectEntity";
+import { IAssignmentPage } from "../api/AssignmentEntity";
 
 const ProjectAsignmentList: NextPage = () => {
-    const [projects, setProjects] = useState<{project:IProject, assignments:IAssignmentPage}[]>();
+    const [projects, setProjects] =
+        useState<{ project: IProject; assignments: IAssignmentPage }[]>();
     const [loading, setLoading] = useState(true);
     useEffect(() => {
         async function fetchProjects() {
             const response: IProjectPage = (await axios.get(pathNames.projects, AxiosConf)).data;
-            const reply:{project:IProject, assignments:IAssignmentPage}[] = [];
-            await Promise.all(response._embedded.projects.map(async (project) => {
-                const assignments: IAssignmentPage = (await axios.get(project._links.assignments.href, AxiosConf)).data;
-                reply.push({project, assignments})
-            }))
-            setProjects(reply)
-            setLoading(false)
+            const reply: { project: IProject; assignments: IAssignmentPage }[] = [];
+            await Promise.all(
+                response._embedded.projects.map(async (project) => {
+                    const assignments: IAssignmentPage = (
+                        await axios.get(project._links.assignments.href, AxiosConf)
+                    ).data;
+                    reply.push({ project, assignments });
+                })
+            );
+            setProjects(reply);
+            setLoading(false);
         }
-        fetchProjects()
-    }, [])
+        fetchProjects();
+    }, []);
 
     if (loading) {
-        return <h3>Loading ...</h3>
+        return <h3>Loading ...</h3>;
     }
 
     console.log(projects);
 
     const projectList = projects!;
-    console.log("Projectlist")
-    console.log(projectList.length)
+    console.log("Projectlist");
+    console.log(projectList.length);
 
     return (
         <>
-            <Accordion defaultActiveKey={['0']} alwaysOpen>
-                { projectList.map((item, index) => {
-                    console.log("item")
-                    console.log(item)
+            <Accordion defaultActiveKey={["0"]} alwaysOpen>
+                {projectList.map((item, index) => {
+                    console.log("item");
+                    console.log(item);
                     return (
                         <AccordionItem key={index} eventKey={`${index}`}>
                             <AccordionHeader>
@@ -53,21 +57,21 @@ const ProjectAsignmentList: NextPage = () => {
                                 </div>
                             </AccordionHeader>
                             <AccordionBody>
-                                { item.assignments._embedded.assignments.map((assignment, index) => {
+                                {item.assignments._embedded.assignments.map((assignment, index) => {
                                     return (
                                         <div key={index}>
                                             <h6>{assignment._links.student.href}</h6>
                                             <p>{assignment.reason}</p>
                                         </div>
-                                    )
+                                    );
                                 })}
                             </AccordionBody>
                         </AccordionItem>
-                    )
+                    );
                 })}
             </Accordion>
         </>
     );
-}
+};
 
 export default ProjectAsignmentList;
