af19c6500414cb1e49a523c17d71b53280254bbd
Kasper Demeyere <kasperdemeyere@hotmail.com>
2022-04-14 17:59:16 +0200

    removed notnull

--- backend/src/main/java/com/osoc6/OSOC6/database/models/Edition.java
+++ backend/src/main/java/com/osoc6/OSOC6/database/models/Edition.java
@@ -11,7 +11,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
 
 
 /**
@@ -42,7 +41,6 @@ public final class Edition implements WeakToEdition {
      * The year of the edition.
      */
     @Basic(optional = false)
-    @NotNull
     @Setter @Getter
     private Integer year;
 
@@ -50,7 +48,6 @@ public final class Edition implements WeakToEdition {
      * Whether the edition is active.
      */
     @Basic(optional = false)
-    @NotNull
     @Setter @Getter
     private Boolean active;
 
