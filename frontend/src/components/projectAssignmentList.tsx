04e1018c82e10d863d8930e2ba5283f225621a61
thvmulle <Thomas.VanMullem@UGent.be>
2022-04-14 11:13:50 +0200

    Prettier

--- frontend/src/components/projectAssignmentList.tsx
+++ frontend/src/components/projectAssignmentList.tsx
@@ -1,36 +1,40 @@
 import { NextPage } from "next";
-import {Accordion, CloseButton, Col, Container, Row} from "react-bootstrap";
+import { Accordion, CloseButton, Col, Container, Row } from "react-bootstrap";
 import AccordionHeader from "react-bootstrap/AccordionHeader";
 import AccordionBody from "react-bootstrap/AccordionBody";
 import AccordionItem from "react-bootstrap/AccordionItem";
 import axios from "axios";
 import pathNames from "../properties/pathNames";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import { AxiosConf } from "../api/requests";
 import { IProject } from "../api/ProjectEntity";
-import {IAssignment, IAssignmentLinks} from "../api/AssignmentEntity";
-import {IStudent} from "../api/StudentEntity";
-import {IUser} from "../api/UserEntity";
-import {IBaseEntity, IPage} from "../api/BaseEntities";
+import { IAssignment, IAssignmentLinks } from "../api/AssignmentEntity";
+import { IStudent } from "../api/StudentEntity";
+import { IUser } from "../api/UserEntity";
+import { IBaseEntity, IPage } from "../api/BaseEntities";
 
-export type Assigments = { assignment: IAssignment, student:IStudent, assigner:IUser }[]
+export type Assigments = { assignment: IAssignment; student: IStudent; assigner: IUser }[];
 
-
-async function getAllEntities<T extends IBaseEntity>(url:string, collectionName: string): Promise<T[]> {
+async function getAllEntities<T extends IBaseEntity>(
+    url: string,
+    collectionName: string
+): Promise<T[]> {
     let fetchedAll: boolean = false;
     let currentPage: number = 0;
     let entities: T[] = [];
 
     while (!fetchedAll) {
         let page: IPage<any>;
-        page = (await axios.get(url, {
-            params: {
-                size: 1000,
-                page: currentPage,
-            },
-            ...AxiosConf
-        })).data;
-        console.log(page)
+        page = (
+            await axios.get(url, {
+                params: {
+                    size: 1000,
+                    page: currentPage,
+                },
+                ...AxiosConf,
+            })
+        ).data;
+        console.log(page);
         entities.push(...page._embedded[collectionName]);
         fetchedAll = currentPage + 1 === page.page.totalPages;
         currentPage++;
@@ -39,15 +43,16 @@ async function getAllEntities<T extends IBaseEntity>(url:string, collectionName:
     return entities;
 }
 
-
-async function getProjectAssignemntData():Promise<{ project: IProject; assignments: Assigments }[]> {
-    const projects: IProject[] = (await getAllEntities<IProject>(pathNames.projects, "projects"));
+async function getProjectAssignemntData(): Promise<
+    { project: IProject; assignments: Assigments }[]
+> {
+    const projects: IProject[] = await getAllEntities<IProject>(pathNames.projects, "projects");
     const reply: { project: IProject; assignments: Assigments }[] = [];
     await Promise.all(
         projects.map(async (project) => {
             const assignmentList: IAssignmentLinks = (
                 await axios.get(project._links.assignments.href, AxiosConf)
-            ).data
+            ).data;
             const assignments: Assigments = [];
             await Promise.all(
                 assignmentList._embedded.assignments.map(async (assignment) => {
@@ -58,20 +63,18 @@ async function getProjectAssignemntData():Promise<{ project: IProject; assignmen
                         await axios.get(assignment._links.assigner.href, AxiosConf)
                     ).data;
                     if (assignment.isValid) {
-                        assignments.push({ assignment, student, assigner } );
+                        assignments.push({ assignment, student, assigner });
                     }
                 })
             );
             reply.push({ project, assignments });
         })
     );
-    return reply
+    return reply;
 }
 
-
 const ProjectAsignmentList: NextPage = () => {
-    const [projects, setProjects] =
-        useState<{ project: IProject; assignments: Assigments }[]>();
+    const [projects, setProjects] = useState<{ project: IProject; assignments: Assigments }[]>();
     const [loading, setLoading] = useState(true);
     useEffect(() => {
         getProjectAssignemntData().then((reply) => {
@@ -89,7 +92,12 @@ const ProjectAsignmentList: NextPage = () => {
     return (
         <>
             <Container>
-                <Accordion defaultActiveKey={["0"]} alwaysOpen className={"w-75 overflow-auto"} style={{'height': '20em'}}>
+                <Accordion
+                    defaultActiveKey={["0"]}
+                    alwaysOpen
+                    className={"w-75 overflow-auto"}
+                    style={{ height: "20em" }}
+                >
                     {projectList.map((item, index) => {
                         console.log("item");
                         console.log(item);
@@ -102,7 +110,7 @@ const ProjectAsignmentList: NextPage = () => {
                                     </div>
                                 </AccordionHeader>
                                 <AccordionBody>
-                                    <AssignmentItem  assignments={item.assignments}/>
+                                    <AssignmentItem assignments={item.assignments} />
                                 </AccordionBody>
                             </AccordionItem>
                         );
@@ -113,30 +121,27 @@ const ProjectAsignmentList: NextPage = () => {
     );
 };
 
-
-export function AssignmentItem( item:{ assignments: Assigments} ) {
-    const assignments = item.assignments
+export function AssignmentItem(item: { assignments: Assigments }) {
+    const assignments = item.assignments;
     const [assign, setAssign] = useState<Assigments>();
-    useEffect(() => setAssign(assignments), [assignments])
+    useEffect(() => setAssign(assignments), [assignments]);
 
     async function removeAssignment(assignment: any) {
-        await axios.delete(assignment.target.value, AxiosConf)
+        await axios.delete(assignment.target.value, AxiosConf);
         if (assign != undefined) {
-            const newAssign:Assigments = [];
+            const newAssign: Assigments = [];
             for (let ass of assign) {
-                if (ass.assignment._links.assignment.href !=  assignment.target.value) {
-                    newAssign.push(ass)
+                if (ass.assignment._links.assignment.href != assignment.target.value) {
+                    newAssign.push(ass);
                 }
             }
-            setAssign(newAssign)
+            setAssign(newAssign);
         }
     }
 
     if (assign != undefined) {
         if (assign.length == 0) {
-            return (
-                <p>No students have been assigned to this project</p>
-            )
+            return <p>No students have been assigned to this project</p>;
         }
 
         return (
@@ -144,23 +149,30 @@ export function AssignmentItem( item:{ assignments: Assigments} ) {
                 {assign.map((assignment, index) => {
                     return (
                         <Container key={index}>
-                            <Row className={"align-items-center"} xs={2} >
+                            <Row className={"align-items-center"} xs={2}>
                                 <Col sm={11}>
                                     <h6>{assignment.student.firstName}</h6>
-                                    <p>Suggested by {assignment.assigner.callName}: <br/> {assignment.assignment.reason}</p>
+                                    <p>
+                                        Suggested by {assignment.assigner.callName}: <br />{" "}
+                                        {assignment.assignment.reason}
+                                    </p>
                                 </Col>
                                 <Col sm={1}>
-                                    <CloseButton aria-label={"Remove student from project"} value={assignment.assignment._links.assignment.href} onClick={(assignment) => removeAssignment(assignment)}/>
+                                    <CloseButton
+                                        aria-label={"Remove student from project"}
+                                        value={assignment.assignment._links.assignment.href}
+                                        onClick={(assignment) => removeAssignment(assignment)}
+                                    />
                                 </Col>
                             </Row>
-                            <hr/>
+                            <hr />
                         </Container>
                     );
                 })}
             </>
-        )
+        );
     }
-    return <p>Loading...</p>
+    return <p>Loading...</p>;
 }
 
 export default ProjectAsignmentList;
