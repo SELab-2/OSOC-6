869e27dd301ec2458483bf8a0785bc9d15528608
Lukas Van Rossem <65233920+lvrossem@users.noreply.github.com>
2022-04-14 15:02:13 +0200

    Update frontend/test/projectList.test.tsx
    Co-authored-by: Anne Depuydt <99727145+andpuydt@users.noreply.github.com>

--- frontend/test/projectList.test.tsx
+++ frontend/test/projectList.test.tsx
@@ -64,8 +64,8 @@ it("Should go to project page when clicking item in list", async () => {
                 totalPages: 1,
             },
         },
-        status: 200,
-        statusText: "",
+        status: StatusCodes.OK,
+        statusText: ReasonPhrases.OK,
         headers: {},
         config: {},
     };
