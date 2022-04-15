1ce8ba48bcb36e7422cc2aa35c65c92b15b03653
Ruben Van Mello <Ruben.VanMello@ugent.be>
2022-04-15 14:57:19 +0200

    patch finally working

--- frontend/src/handlers/profileHandler.ts
+++ frontend/src/handlers/profileHandler.ts
@@ -1,10 +1,26 @@
-import axios from "axios";
-import { AxiosFormConfig } from "../api/requests";
+import axios, { AxiosResponse } from "axios";
 import Router from "next/router";
+import applicationPaths from "../properties/applicationPaths";
 import apiPaths from "../properties/apiPaths";
 
-export async function profileDeleteHandler(url: string) {
-    const response = await axios.delete(url, AxiosFormConfig);
+export const AxiosFormConfig = {
+    baseURL: apiPaths.base,
+    headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+    },
+};
+
+export async function profileDeleteHandler(event: any) {
+    const response = await axios.delete(event.target.value, AxiosFormConfig);
     // redirect to login
-    await Router.push(apiPaths.login);
+    if (response.status == 204) {
+        await Router.push(applicationPaths.login);
+    }
+}
+
+export async function profileSaveHandler(
+    url: string,
+    callname: string
+): Promise<AxiosResponse<any>> {
+    return await axios.patch(url, { callName: callname }, AxiosFormConfig);
 }
--- frontend/src/properties/applicationPaths.ts
+++ frontend/src/properties/applicationPaths.ts
@@ -1,5 +1,8 @@
 const applicationPaths = {
     projectCreation: "projects/create",
+    login: "/login",
+    changePassword: "/changePassword",
+    changeEmail: "/changeEmail",
 };
 
 export default applicationPaths;
