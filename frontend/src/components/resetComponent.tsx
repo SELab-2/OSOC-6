295f5a913e74af3b6bb819b487010e0426425a79
Ruben Van Mello <Ruben.VanMello@ugent.be>
2022-04-23 16:31:59 +0200

    prettifier

--- frontend/src/components/resetComponent.tsx
+++ frontend/src/components/resetComponent.tsx
@@ -54,7 +54,7 @@ export const ResetComponent = (props: any) => {
     return (
         <Container className={styles.reset_component} data-testid="reset-component">
             <h4>{capitalize(t("reset " + props.name))}</h4>
-            <Form.Label>{capitalize(t("new"+ props.name))}</Form.Label>
+            <Form.Label>{capitalize(t("new" + props.name))}</Form.Label>
             <FormControl id="" data-testid="reset-input-1" type={props.name} onChange={onChangeFirstEntry} />
             <Form.Label>{capitalize(t("repeat new" + props.name))}</Form.Label>
             <FormControl id="" data-testid="reset-input-2" type={props.name} onChange={onChangeSecondEntry} />
