20e224165b749e43f819181d0571c31b48da24e1
andpuydt <anne.depuydt@ugent.be>
2022-04-24 15:17:44 +0200

    Add suggestions

--- frontend/src/components/studentInfo.tsx
+++ frontend/src/components/studentInfo.tsx
@@ -5,11 +5,9 @@ import useSWR from "swr";
 import { getAllStudentInfo } from "../api/calls/studentCalls";
 import { capitalize } from "../utility/stringUtil";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
-import { suggest } from "../handlers/suggestionHandler";
 import styles from "../styles/studentInfo.module.css";
-import {SuggestionStrategy} from "../api/entities/SuggestionEntity";
-import {CustomDialogContent} from "./dialog";
-
+import { SuggestionStrategy } from "../api/entities/SuggestionEntity";
+import { CustomDialogContent } from "./suggestionModal";
 
 export function StudentInfo() {
     const { t } = useTranslation("common");
@@ -30,112 +28,111 @@ export function StudentInfo() {
 
     return (
         <div>
-        <div>
-            <div className="row">
-                <div className="col-sm-6">
-                    <h1>{data.student.callName}</h1>
-                </div>
-                <div className="col-sm-6">
-                    <ListGroup className="list-group-horizontal" as="ul">
-                        {data.student.skills.map((skill) => (
-                            <ListGroupItem key={skill} className={styles.skillStyle}>
-                                <p>{skill}</p>
-                            </ListGroupItem>
-                        ))}
-                    </ListGroup>
-                </div>
-            </div>
-            <br />
-            <h2>{capitalize(t("student suggestions"))}</h2>
-            <ListGroup as="ul">
-                {data.suggestions
-                    .map((suggestion) => ({
-                        suggestion,
-                        suggestionId: suggestion.suggestion._links.self.href.split(apiPaths.base)[1],
-                    }))
-                    .map(({ suggestion, suggestionId }) => (
-                        <ListGroup.Item key={suggestionId} as={"li"}>
-                            <div>
-                                {suggestion.coach.callName}: {suggestion.suggestion.reason}
-                            </div>
-                        </ListGroup.Item>
-                    ))}
-            </ListGroup>
-            <br />
-            <h2>{capitalize(t("student about"))}</h2>
-            <a href={data.student.curriculumVitaeURI}>{capitalize(t("student cv"))}</a> <br />
-            <a href={data.student.portfolioURI}>{capitalize(t("student portfolio"))}</a> <br />
-            <a href={data.student.motivationURI}>{capitalize(t("student motivation"))}</a> <br />
-            <br />
-            <h2>{capitalize(t("student personal details"))}</h2>
             <div>
-                {capitalize(t("student gender"))}: {data.student.gender.toLowerCase()} {t("student pronouns")}{" "}
-                {data.student.pronounsType.toLowerCase()}
-            </div>
-            <div>
-                {capitalize(t("student native language"))}: {data.student.mostFluentLanguage}
-            </div>
-            <div>
-                {capitalize(t("student english proficiency"))}:{" "}
-                {data.student.englishProficiency.toLowerCase()}
-            </div>
-            <div>
-                {capitalize(t("student phone number"))}: {data.student.phoneNumber}
-            </div>
-            <div>
-                {capitalize(t("student email"))}: {data.student.email}
-            </div>
-            <br />
-            <h2>{capitalize(t("student education"))}</h2>
-            <div>
-                {capitalize(t("student studies"))}: {data.student.studies.join(", ")}
-            </div>
-            <div>
-                {capitalize(t("student institution"))}: {data.student.institutionName}
-            </div>
-            <div>
-                {capitalize(t("student current diploma"))}: {data.student.currentDiploma}
-            </div>
-            <div>
-                {capitalize(t("student degree year"))}: {data.student.yearInCourse}{" "}
-                {t("student degree duration")} {data.student.durationCurrentDegree}
-            </div>
-            <div>
-                {capitalize(t("student applied for"))}: {data.student.skills.join(", ")}
-            </div>
-            <div>
-                {capitalize(t("student osoc experience"))}: {t(experiences)}
-            </div>
-        </div>
-        <footer className="studentfooter py-3 fixed-bottom">
-            <div className="container">
                 <div className="row">
-                    <div className="col-sm">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-sm">
-                                    <button type="button" className="btn btn-outline"
-                                            data-mdb-toggle="modal" data-mdb-target="#exampleModal"
-                                            style={{color: "#1DE1AE", borderColor: "#1DE1AE", width: 150}}>Suggest yes</button>
+                    <div className="col-sm-6">
+                        <h1>{data.student.callName}</h1>
+                    </div>
+                    <div className="col-sm-6">
+                        <ListGroup className="list-group-horizontal" as="ul">
+                            {data.student.skills.map((skill) => (
+                                <ListGroupItem key={skill} className={styles.skillStyle}>
+                                    <p>{skill}</p>
+                                </ListGroupItem>
+                            ))}
+                        </ListGroup>
+                    </div>
+                </div>
+                <br />
+                <h2>{capitalize(t("student suggestions"))}</h2>
+                <ListGroup as="ul">
+                    {data.suggestions
+                        .map((suggestion) => ({
+                            suggestion,
+                            suggestionId: suggestion.suggestion._links.self.href.split(apiPaths.base)[1],
+                        }))
+                        .map(({ suggestion, suggestionId }) => (
+                            <ListGroup.Item key={suggestionId} as={"li"}>
+                                <div>
+                                    {suggestion.coach.callName}: {suggestion.suggestion.reason}
                                 </div>
-                                <div className="col-sm">
-                                    <button type="button" className="btn btn-outline"
-                                            style={{color: "#FCB70F", borderColor: "#FCB70F", width: 150}}>Suggest maybe</button>
+                            </ListGroup.Item>
+                        ))}
+                </ListGroup>
+                <br />
+                <h2>{capitalize(t("student about"))}</h2>
+                <a href={data.student.curriculumVitaeURI}>{capitalize(t("student cv"))}</a> <br />
+                <a href={data.student.portfolioURI}>{capitalize(t("student portfolio"))}</a> <br />
+                <a href={data.student.motivationURI}>{capitalize(t("student motivation"))}</a> <br />
+                <br />
+                <h2>{capitalize(t("student personal details"))}</h2>
+                <div>
+                    {capitalize(t("student gender"))}: {data.student.gender.toLowerCase()}{" "}
+                    {t("student pronouns")} {data.student.pronounsType.toLowerCase()}
+                </div>
+                <div>
+                    {capitalize(t("student native language"))}: {data.student.mostFluentLanguage}
+                </div>
+                <div>
+                    {capitalize(t("student english proficiency"))}:{" "}
+                    {data.student.englishProficiency.toLowerCase()}
+                </div>
+                <div>
+                    {capitalize(t("student phone number"))}: {data.student.phoneNumber}
+                </div>
+                <div>
+                    {capitalize(t("student email"))}: {data.student.email}
+                </div>
+                <br />
+                <h2>{capitalize(t("student education"))}</h2>
+                <div>
+                    {capitalize(t("student studies"))}: {data.student.studies.join(", ")}
+                </div>
+                <div>
+                    {capitalize(t("student institution"))}: {data.student.institutionName}
+                </div>
+                <div>
+                    {capitalize(t("student current diploma"))}: {data.student.currentDiploma}
+                </div>
+                <div>
+                    {capitalize(t("student degree year"))}: {data.student.yearInCourse}{" "}
+                    {t("student degree duration")} {data.student.durationCurrentDegree}
+                </div>
+                <div>
+                    {capitalize(t("student applied for"))}: {data.student.skills.join(", ")}
+                </div>
+                <div>
+                    {capitalize(t("student osoc experience"))}: {t(experiences)}
+                </div>
+            </div>
+            <footer className="studentfooter py-3 fixed-bottom">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm">
+                            <div className="container">
+                                <div className="row">
+                                    <div className="col-sm">
+                                        <CustomDialogContent suggestion={SuggestionStrategy.yes}
+                                             style={{ color: "#1DE1AE", borderColor: "#1DE1AE", width: 150 }}
+                                             studentUrl={data.student._links.self.href}/>
+                                    </div>
+                                    <div className="col-sm">
+                                        <CustomDialogContent suggestion={SuggestionStrategy.maybe}
+                                             style={{ color: "#FCB70F", borderColor: "#FCB70F", width: 150 }}
+                                             studentUrl={data.student._links.self.href}/>
+                                    </div>
+                                    <div className="col-sm">
+                                        <CustomDialogContent suggestion={SuggestionStrategy.no}
+                                             style={{ color: "#F14A3B", borderColor: "#F14A3B", width: 150 }}
+                                             studentUrl={data.student._links.self.href}/>
+                                    </div>
                                 </div>
-                                <div className="col-sm">
-                                    <button type="button" className="btn btn-outline"
-                                            style={{color: "#F14A3B", borderColor: "#F14A3B", width: 150}}>Suggest no</button>
-                                </div>
-                                <CustomDialogContent/>
                             </div>
                         </div>
-                    </div>
-                    <div className="col-sm">
-                        Add definite decision
+                        <div className="col-sm">Add definite decision</div>
                     </div>
                 </div>
-            </div>
-        </footer>
+            </footer>
         </div>
     );
 }
---
+++ frontend/src/components/suggestionModal.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
+import { Suggestion, SuggestionStrategy } from "../api/entities/SuggestionEntity";
+import { Field, Form, Formik } from "formik";
+import axios, {AxiosResponse} from "axios";
+import apiPaths from "../properties/apiPaths";
+import { AxiosConf } from "../api/calls/baseCalls";
+import { IUser } from "../api/entities/UserEntity";
+
+export function CustomDialogContent(props: {suggestion: SuggestionStrategy, style: any, studentUrl: string}) {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleShow = () => setShowModal(true);
+    const handleClose = () => setShowModal(false);
+
+    async function submitSuggestionHandler(values:{reason: string}) {
+        // Get the logged in user
+        const userResponse: AxiosResponse = await axios.get(apiPaths.ownUser, AxiosConf);
+        const user: IUser = userResponse.data;
+        const suggestion = new Suggestion(props.suggestion, values.reason, user._links.self.href, props.studentUrl);
+
+        await axios.post(apiPaths.suggestions, suggestion, AxiosConf);
+        handleClose();
+    }
+
+    return (
+        <>
+            <Button variant="btn-outline" onClick={handleShow} style={props.style}>
+                Suggest {props.suggestion.toLowerCase()}
+            </Button>
+
+            <Modal show={showModal} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Suggest {props.suggestion}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div>Reason for the suggestion: </div>
+                    <Formik
+                        initialValues={{ reason: "" }}
+                        onSubmit={submitSuggestionHandler}
+                    >
+                        <Form>
+                            <Field
+                                className="form-control mb-2"
+                                type="text"
+                                data-testid="suggestion-reason"
+                                name="reason"
+                                required
+                            />
+                            <button className="btn btn-primary" type="submit"
+                                    style={{float: "right"}}>
+                                Confirm suggestion
+                            </button>
+                        </Form>
+                    </Formik>
+                </Modal.Body>
+            </Modal>
+        </>
+    );
+}
