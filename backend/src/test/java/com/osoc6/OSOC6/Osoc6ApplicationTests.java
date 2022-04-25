48a4154849175d42a015d822c3679946ec51e133
Kasper Demeyere <kasperdemeyere@hotmail.com>
2022-04-25 16:21:37 +0200

    (broken) begin base admin test

--- backend/src/test/java/com/osoc6/OSOC6/Osoc6ApplicationTests.java
+++ backend/src/test/java/com/osoc6/OSOC6/Osoc6ApplicationTests.java
@@ -1,13 +1,37 @@
 package com.osoc6.OSOC6;
 
+import com.osoc6.OSOC6.database.models.UserEntity;
+import com.osoc6.OSOC6.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * Auto generated class that tests if the context can be loaded.
  */
 @SpringBootTest
-class Osoc6ApplicationTests {
+class Osoc6ApplicationTests extends BaseTestPerformer<UserEntity, Long, UserRepository> {
+    @Autowired
+    private UserRepository userRepository;
+
+    private String baseAdminEmail;
+
+    @Override
+    public Long get_id(final UserEntity entity) {
+        return entity.getId();
+    }
+
+    @Override
+    public UserRepository get_repository() {
+        return userRepository;
+    }
+
+    @Override
+    public void setUpRepository() { }
+
+    @Override
+    public void removeSetUpRepository() { }
+
     @Test
     public void test() {
         Osoc6Application.main(new String[]{
