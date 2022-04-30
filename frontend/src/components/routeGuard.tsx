c6a90c75c341871a8b5980e62b5f6b4b0d59282b
Jitse De Smet <jitse@reedonk8.be>
2022-04-30 20:23:46 +0200

    (broken) minor speedup

--- frontend/src/components/routeGuard.tsx
+++ frontend/src/components/routeGuard.tsx
@@ -1,12 +1,13 @@
 import applicationPaths from "../properties/applicationPaths";
 import { useCurrentUser } from "../api/calls/userCalls";
-import { useRouter } from "next/router";
+import Router, { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { pathIsAuthException } from "../utility/pathUtil";
 import { getAllEditionsFromPage, useCurrentEdition } from "../api/calls/editionCalls";
 import useSWR from "swr";
 import { getQueryUrlFromParams } from "../api/calls/baseCalls";
 import apiPaths from "../properties/apiPaths";
+import { data } from "browserslist";
 
 export default function RouteGuard({ children }: any) {
     const router = useRouter();
@@ -16,66 +17,54 @@ export default function RouteGuard({ children }: any) {
     const { error: userError } = useCurrentUser(true);
     const userErrorMsg = userError?.message;
 
-    const curEdition = useCurrentEdition();
-    const curEditionName = curEdition?.name;
+    let cachedEditionName: string | undefined;
+    try {
+        cachedEditionName = localStorage.getItem("edition") || undefined;
+    } catch (e: any) {
+        // localStorage was not yet defined
+    }
+
+    const routerPath = router.asPath;
+    const curEditionName = (router.query as { edition?: string }).edition;
 
     const { data: availableEditions } = useSWR(
-        !curEditionName ? getQueryUrlFromParams(apiPaths.editions, { sort: "year" }) : null,
+        !cachedEditionName ? getQueryUrlFromParams(apiPaths.editions, { sort: "year" }) : null,
         getAllEditionsFromPage
     );
     const latestEdition = availableEditions?.at(0);
     const latestEditionName = latestEdition?.name;
 
-    let cachedEditionName: string | undefined;
-    try {
-        cachedEditionName = localStorage?.getItem("edition") || undefined;
-    } catch (e: any) {
-        // CachedEdition is not yet defined here
+    if (curEditionName && cachedEditionName !== curEditionName) {
+        localStorage.setItem("edition", curEditionName);
     }
-
-    if (curEdition && cachedEditionName !== curEditionName) {
-        console.log("edition is set");
-        localStorage.setItem("edition", curEdition.name);
-    }
-
-    // console.log(cachedEditionName);
-    // console.log(userErrorMsg)
-    // console.log(curEdition)
-    // console.log(latestEditionName)
 
     useEffect(() => {
         // Check the authentication of the current path
-        const authException = pathIsAuthException(router.asPath);
+        const authException = pathIsAuthException(routerPath);
 
         if (!authException && userErrorMsg) {
-            router
-                .replace({
-                    pathname: applicationPaths.login,
-                    query: { returnUrl: router.asPath },
-                })
-                .catch(console.log);
+            Router.replace({
+                pathname: applicationPaths.login,
+                query: { returnUrl: routerPath },
+            }).catch(console.log);
         }
 
-        if (cachedEditionName && !router.query.edition) {
-            router
-                .replace({
-                    query: { edition: cachedEditionName },
-                })
-                .catch(console.log);
+        if (cachedEditionName && !curEditionName) {
+            Router.replace({
+                query: { edition: cachedEditionName },
+            }).catch(console.log);
         }
 
         if (!cachedEditionName && !curEditionName && latestEditionName) {
-            router
-                .replace({
-                    query: {
-                        edition: latestEditionName,
-                    },
-                })
-                .catch(console.log);
+            Router.replace({
+                query: {
+                    edition: latestEditionName,
+                },
+            }).catch(console.log);
         }
 
         setAuthorized(authException || (!userErrorMsg && !!curEditionName));
-    }, [router.asPath, userErrorMsg, curEditionName, cachedEditionName, latestEditionName]);
+    }, [routerPath, userErrorMsg, curEditionName, cachedEditionName, latestEditionName]);
 
     if (availableEditions && availableEditions.length === 0) {
         if (router.pathname !== "/" + applicationPaths.home) {
