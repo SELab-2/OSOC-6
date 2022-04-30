fc384c7007fe14e22ef0dc5762982d8ab07002a3
Jitse De Smet <jitse@reedonk8.be>
2022-04-30 22:30:08 +0200

    swr config

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,12 +2,17 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import "bootstrap/dist/css/bootstrap.css";
 import RouteGuard from "../components/routeGuard";
+import { SWRConfig } from "swr";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <RouteGuard>
-            <Component {...pageProps} />
-        </RouteGuard>
+        <SWRConfig value={{
+            refreshInterval: 20
+        }}>
+            <RouteGuard>
+                <Component {...pageProps} />
+            </RouteGuard>
+        </SWRConfig>
     );
 }
 
