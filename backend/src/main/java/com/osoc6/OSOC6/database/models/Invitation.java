ad289bcafb47b0e3db69d92f546a07b2fe905c94
Kasper Demeyere <kasperdemeyere@hotmail.com>
2022-05-03 15:43:44 +0200

    change token from base64keygen to randomUUID

--- backend/src/main/java/com/osoc6/OSOC6/database/models/Invitation.java
+++ backend/src/main/java/com/osoc6/OSOC6/database/models/Invitation.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.ReadOnlyProperty;
-import org.springframework.security.crypto.keygen.Base64StringKeyGenerator;
 
 import javax.persistence.Basic;
 import javax.persistence.Column;
@@ -20,6 +19,7 @@ import javax.persistence.ManyToOne;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.Calendar;
+import java.util.UUID;
 
 /**
  * The database entity for an Invitation.
@@ -43,7 +43,7 @@ public final class Invitation implements WeakToEdition {
     @Basic(optional = false)
     @Column(unique = true) @ReadOnlyProperty
     @Getter
-    private final String token = new Base64StringKeyGenerator().generateKey();
+    private final String token = UUID.randomUUID().toString();
 
     /**
      * The timestamp of the invitation.
