82994b3f30ea296042207f24933ff9c41b96825e
Jitse De Smet <35114273+jitsedesmet@users.noreply.github.com>
2022-05-04 18:42:38 +0200

    CommunicationTemplate creation component (#388)
    * implement form
    
    * implement push
    
    * add tests
    
    * use translations
    
    * resolve changes requested by @ruvmello
    
    * resolve change requested by @ruvmello

---
+++ frontend/src/pages/communicationTemplate/create.tsx
@@ -0,0 +1,51 @@
+import { Field, Form, Formik } from "formik";
+import { CommunicationTemplateEntity } from "../../api/entities/CommunicationTemplateEntity";
+import { createCommunicationTemplateSubmitHandler } from "../../handlers/createCommunicationTemplateSubmitHandler";
+import { useRouter } from "next/router";
+import useTranslation from "next-translate/useTranslation";
+import { capitalize } from "../../utility/stringUtil";
+
+export default function CommunicationTemplateCreate() {
+    const { t } = useTranslation("common");
+    const router = useRouter();
+    const initialValues: CommunicationTemplateEntity = new CommunicationTemplateEntity("", "");
+
+    return (
+        <div data-testid="communication-template-create">
+            <h1 className="capitalize">{t("create communication template")}</h1>
+            <Formik
+                initialValues={initialValues}
+                onSubmit={(values) => createCommunicationTemplateSubmitHandler(values, router)}
+            >
+                {({ values, setFieldValue }) => (
+                    <Form>
+                        <label className="capitalize" htmlFor="communicationTemplateNameField">
+                            {t("name") + ":"}
+                        </label>
+                        <Field
+                            type="text"
+                            name="name"
+                            required
+                            id="communicationTemplateNameField"
+                            data-testid="name"
+                        />
+
+                        <textarea
+                            placeholder={capitalize(t("template placeholder"))}
+                            className="form-control"
+                            style={{ height: "100px" }}
+                            id="communicationTemplateTemplateField"
+                            data-testid="template"
+                            value={values.template}
+                            onChange={(event) => setFieldValue("template", event.target.value)}
+                        />
+
+                        <button className="capitalize" data-testid="submit" type="submit">
+                            {t("confirm")}
+                        </button>
+                    </Form>
+                )}
+            </Formik>
+        </div>
+    );
+}
