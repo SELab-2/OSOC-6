0b869785d889fb1e92e995c0556586bbaf001018
andpuydt <anne.depuydt@ugent.be>
2022-05-18 22:16:31 +0200

    Add url to template content + fix tests with @jitsedesmet

--- frontend/src/components/user/invitationButton.tsx
+++ frontend/src/components/user/invitationButton.tsx
@@ -14,17 +14,19 @@ import mailTo from "../../utility/mailTo";
 import useSWR from "swr";
 import { getCommunicationTemplateByName } from "../../api/calls/communicationTemplateCalls";
 import { emptyCommunicationTemplate } from "../../api/entities/CommunicationTemplateEntity";
+import useEdition from "../../hooks/useGlobalEdition";
 
 export default function InvitationButton() {
     const { t } = useTranslation("common");
     const transformer = useEditionApplicationPathTransformer();
     const { user, error } = useCurrentUser(true);
-    let { data: template, error: templateError } = useSWR(
-        "yes", // TODO: change to invite
+    const [receivedEditionUrl, setCurrentEditionUrl] = useEdition();
+    const { data, error: templateError } = useSWR(
+        "Invite",
         getCommunicationTemplateByName
     );
 
-    template = template || emptyCommunicationTemplate;
+    const template = data || emptyCommunicationTemplate;
 
     if (templateError) {
         return null;
@@ -33,23 +35,27 @@ export default function InvitationButton() {
     const invitationField = document.getElementById("invitation-url");
 
     async function onClick() {
-        // Get an edition : TODO make this the current edition
-        const editions: IEdition[] = await getAllEditionsFromPage(apiPaths.editions);
-        const edition: IEdition = editions[0];
-
         // Create an invitation
         // We can never call this function if user is undefined.
-        const invitation = new Invitation(user!._links.self.href, edition._links.self.href);
+        const invitation = new Invitation(user!._links.self.href, receivedEditionUrl!);
         const postedInvitation = await createInvitation(invitation);
 
         const url: string = getQueryUrlFromParams(applicationPaths.registration, {
             invitationToken: postedInvitation.token,
         });
 
+        const registrationUrl = applicationPaths.base + "/" + transformer(url);
+
         if (invitationField !== null) {
-            invitationField.setAttribute("value", applicationPaths.base + "/" + transformer(url));
+            invitationField.setAttribute("value", registrationUrl);
             invitationField.style.display = "inline";
         }
+
+        document.location.href = mailTo({
+            body: template.template + "\n" + registrationUrl,
+            subject: template.subject,
+            recipients: undefined,
+        })
     }
 
     return (
@@ -57,11 +63,6 @@ export default function InvitationButton() {
             <Button
                 data-testid="invite-button"
                 onClick={onClick}
-                href={mailTo({
-                    body: template.template,
-                    subject: template.subject,
-                    recipients: undefined,
-                })}
             >
                 {capitalize(t("invite user"))}
             </Button>
