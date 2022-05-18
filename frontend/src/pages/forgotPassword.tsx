020a05ee7518f4b3c7d805a50b435e1f79ef8d3b
Kasper Demeyere <kasperdemeyere@hotmail.com>
2022-05-18 17:46:16 +0200

    add title to common

--- frontend/src/pages/forgotPassword.tsx
+++ frontend/src/pages/forgotPassword.tsx
@@ -3,17 +3,21 @@ import { ForgotComponent } from "../components/util/forgotComponent";
 import { forgotPasswordSubmitHandler } from "../handlers/forgotPasswordSubmitHandler";
 import NavBar from "../components/util/navBar";
 import styles from "../styles/forgotPassword.module.css";
+import useTranslation from "next-translate/useTranslation";
+import { capitalize } from "../utility/stringUtil";
 
 /**
  * The forgot password page, where a user can enter their email to request a password reset.
  */
 const ForgotPassword: NextPage = () => {
+    const { t } = useTranslation("common");
+
     return (
         <main>
             <NavBar />
             <div className={styles.forgot_full_div}>
                 <div className="d-flex justify-content-center align-items-center flex-column">
-                    <h2 className="mt-5">Forgot password</h2>
+                    <h2 className="mt-5">{capitalize(t("forgot password title"))}</h2>
                     <ForgotComponent submitHandler={(form) => forgotPasswordSubmitHandler(form)} />
                 </div>
             </div>
