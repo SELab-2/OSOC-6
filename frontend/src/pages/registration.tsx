9d60a020009a63c0b19989721850f71c52ecf3e1
Thomas Van Mullem <99752211+thvmulle@users.noreply.github.com>
2022-05-20 23:14:06 +0200

    Add layout to all change... pages (#495)
    -> changeEmail, changePassword, forgotPassword, registration, resetPassword

--- frontend/src/pages/registration.tsx
+++ frontend/src/pages/registration.tsx
@@ -2,32 +2,32 @@ import { NextPage } from "next";
 import RegistrationForm from "../components/user/registrationForm";
 import NavBar from "../components/util/navBar";
 import { capitalize } from "../utility/stringUtil";
-import styles from "../styles/registration.module.css";
+import styles from "../styles/loginForm.module.css";
 import { Col, Row } from "react-bootstrap";
 import useTranslation from "next-translate/useTranslation";
+import { Background } from "../components/util/background";
 
 const Registration: NextPage = () => {
     const { t } = useTranslation("common");
 
     return (
-        <main>
+        <div>
+            <Background />
             <NavBar />
-            <div className={styles.registration_full_div}>
-                <Row className={styles.registration_row}>
-                    <Col>
-                        <h2 style={{ marginTop: "100px", marginLeft: "100px" }}>
-                            {capitalize(t("let's get started"))}
-                        </h2>
-                    </Col>
-                    <Col>
-                        <div className={styles.registration_div}>
+            <div className={styles.login_full_div}>
+                <div className={styles.login_row}>
+                    <div className={styles.login_floating_text_col}>
+                        <h2 className={styles.login_floating_text}>{capitalize(t("let's get started"))}</h2>
+                    </div>
+                    <div className={styles.login_col}>
+                        <div className={styles.login_div}>
                             <h3>{capitalize(t("registration"))}</h3>
                             <RegistrationForm />
                         </div>
-                    </Col>
-                </Row>
+                    </div>
+                </div>
             </div>
-        </main>
+        </div>
     );
 };
 
