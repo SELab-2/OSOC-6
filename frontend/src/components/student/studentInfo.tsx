87d68625fe1f7276e86a397d7f6d5394d1afe61a
andpuydt <anne.depuydt@ugent.be>
2022-05-21 22:01:04 +0200

    Prettier

--- frontend/src/components/student/studentInfo.tsx
+++ frontend/src/components/student/studentInfo.tsx
@@ -1,11 +1,11 @@
 import useTranslation from "next-translate/useTranslation";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 import apiPaths from "../../properties/apiPaths";
-import {capitalize} from "../../utility/stringUtil";
-import {Button, Col, ListGroup, Row, Toast, ToastContainer} from "react-bootstrap";
-import {SuggestionStrategy} from "../../api/entities/SuggestionEntity";
-import {SuggestionModal} from "../suggestion/suggestionModal";
-import {StudentStatus} from "./studentStatus";
+import { capitalize } from "../../utility/stringUtil";
+import { Button, Col, ListGroup, Row, Toast, ToastContainer } from "react-bootstrap";
+import { SuggestionStrategy } from "../../api/entities/SuggestionEntity";
+import { SuggestionModal } from "../suggestion/suggestionModal";
+import { StudentStatus } from "./studentStatus";
 import {
     emptyStudent,
     englishProficiencyAsString,
@@ -14,33 +14,33 @@ import {
 } from "../../api/entities/StudentEntity";
 import SkillBadge from "../util/skillBadge";
 import useSWR from "swr";
-import {extractIdFromStudentUrl, deleteStudent, getStudentOnUrl} from "../../api/calls/studentCalls";
-import {getAllSuggestionsFromLinks} from "../../api/calls/suggestionCalls";
+import { extractIdFromStudentUrl, deleteStudent, getStudentOnUrl } from "../../api/calls/studentCalls";
+import { getAllSuggestionsFromLinks } from "../../api/calls/suggestionCalls";
 import SuggestionListItem from "../suggestion/suggestionListItem";
 import applicationPaths from "../../properties/applicationPaths";
-import {useEditionApplicationPathTransformer} from "../../hooks/utilHooks";
+import { useEditionApplicationPathTransformer } from "../../hooks/utilHooks";
 import styles from "../../styles/studentOverview.module.css";
 import Image from "next/image";
-import {StatusCodes} from "http-status-codes";
+import { StatusCodes } from "http-status-codes";
 import timers from "../../properties/timers";
-import {useState} from "react";
-import {getParamsFromQueryUrl, getQueryUrlFromParams} from "../../api/calls/baseCalls";
-import {useCurrentAdminUser} from "../../hooks/useCurrentUser";
-import {getStudentQueryParamsFromQuery} from "./studentFilterComponent";
+import { useState } from "react";
+import { getParamsFromQueryUrl, getQueryUrlFromParams } from "../../api/calls/baseCalls";
+import { useCurrentAdminUser } from "../../hooks/useCurrentUser";
+import { getStudentQueryParamsFromQuery } from "./studentFilterComponent";
 
 /**
  * Give an overview of all the studentinfo
  */
 export function StudentInfo() {
-    const {t} = useTranslation("common");
+    const { t } = useTranslation("common");
     const router = useRouter();
     const transformer = useEditionApplicationPathTransformer();
     const isAdmin = useCurrentAdminUser();
-    const {id} = router.query as { id: string };
+    const { id } = router.query as { id: string };
     const [show, setShow] = useState<boolean>(false);
 
-    let {data: student, error: studentError} = useSWR(apiPaths.students + "/" + id, getStudentOnUrl);
-    let {data: suggestions, error: suggestionsError} = useSWR(
+    let { data: student, error: studentError } = useSWR(apiPaths.students + "/" + id, getStudentOnUrl);
+    let { data: suggestions, error: suggestionsError } = useSWR(
         student ? student._links.suggestions.href : null,
         getAllSuggestionsFromLinks
     );
@@ -64,7 +64,7 @@ export function StudentInfo() {
         motivation = (
             <>
                 <a href={student.motivationURI}>{capitalize(t("motivation"))}</a>
-                <br/>
+                <br />
             </>
         );
     }
@@ -93,18 +93,19 @@ export function StudentInfo() {
 
     return (
         <div className={"h-100"}>
-            <div className={"overflow-auto p-3"} style={{height: "calc(100% - 4rem)"}}>
-                {isAdmin &&
-                    <div className="row w-100" style={{paddingBottom: 15}}>
+            <div className={"overflow-auto p-3"} style={{ height: "calc(100% - 4rem)" }}>
+                {isAdmin && (
+                    <div className="row w-100" style={{ paddingBottom: 15 }}>
                         <Button
                             variant="btn-outline"
                             data-testid="open-communication"
-                            style={{color: "white", borderColor: "white"}}
+                            style={{ color: "white", borderColor: "white" }}
                             onClick={openCommunications}
                         >
                             {capitalize(t("communication"))}
                         </Button>
-                    </div>}
+                    </div>
+                )}
                 <h1>
                     {student.callName}
                     {isAdmin && (
@@ -114,42 +115,42 @@ export function StudentInfo() {
                                 data-testid="edit-student"
                                 href={transformer(
                                     "/" +
-                                    applicationPaths.students +
-                                    "/" +
-                                    extractIdFromStudentUrl(student._links.self.href) +
-                                    applicationPaths.studentEdit.split(applicationPaths.studentInfo)[1]
+                                        applicationPaths.students +
+                                        "/" +
+                                        extractIdFromStudentUrl(student._links.self.href) +
+                                        applicationPaths.studentEdit.split(applicationPaths.studentInfo)[1]
                                 )}
                             >
-                                <Image alt="" src={"/resources/edit.svg"} width="15" height="15"/>
+                                <Image alt="" src={"/resources/edit.svg"} width="15" height="15" />
                             </a>
                             <a
                                 className="ms-2 clickable"
                                 onClick={deleteStudentOnClick}
                                 data-testid="delete-student"
                             >
-                                <Image alt="" src={"/resources/delete.svg"} width="15" height="15"/>
+                                <Image alt="" src={"/resources/delete.svg"} width="15" height="15" />
                             </a>
                         </>
                     )}
                 </h1>
                 <div className={styles.student_skills}>
                     {student.skills.map((skill) => (
-                        <SkillBadge skill={skill} key={skill}/>
+                        <SkillBadge skill={skill} key={skill} />
                     ))}
                 </div>
-                <br/>
+                <br />
                 <h2>{capitalize(t("suggestions"))}</h2>
                 <ListGroup as="ul">
                     {suggestions.map((suggestion) => (
-                        <SuggestionListItem suggestion={suggestion} key={suggestion._links.self.href}/>
+                        <SuggestionListItem suggestion={suggestion} key={suggestion._links.self.href} />
                     ))}
                 </ListGroup>
-                <br/>
+                <br />
                 <h2>{capitalize(t("about"))}</h2>
-                <a href={student.curriculumVitaeURI}>{capitalize(t("cv"))}</a> <br/>
-                <a href={student.portfolioURI}>{capitalize(t("portfolio"))}</a> <br/>
+                <a href={student.curriculumVitaeURI}>{capitalize(t("cv"))}</a> <br />
+                <a href={student.portfolioURI}>{capitalize(t("portfolio"))}</a> <br />
                 {motivation}
-                <br/>
+                <br />
                 <h2>{capitalize(t("personal details"))}</h2>
                 <div>
                     {capitalize(t("gender"))}: {capitalize(t(genderAsString[student.gender]))}{" "}
@@ -168,7 +169,7 @@ export function StudentInfo() {
                 <div>
                     {capitalize(t("email"))}: {student.email}
                 </div>
-                <br/>
+                <br />
                 <h2>{capitalize(t("education"))}</h2>
                 <div>
                     {capitalize(t("studies"))}: {student.studies.join(", ")}
@@ -191,7 +192,7 @@ export function StudentInfo() {
                     {capitalize(t(osocExperienceAsString[student.osocExperience]))}
                 </div>
             </div>
-            <footer className={"py-3 position-sticky bottom-0"} style={{backgroundColor: "#1b1a31"}}>
+            <footer className={"py-3 position-sticky bottom-0"} style={{ backgroundColor: "#1b1a31" }}>
                 <Row>
                     <ToastContainer position="bottom-end">
                         <Toast
@@ -212,28 +213,28 @@ export function StudentInfo() {
                             <div className="col-sm">
                                 <SuggestionModal
                                     suggestion={SuggestionStrategy.yes}
-                                    style={{color: "#1DE1AE", borderColor: "#1DE1AE", width: 150}}
+                                    style={{ color: "#1DE1AE", borderColor: "#1DE1AE", width: 150 }}
                                     studentUrl={student._links.self.href}
                                 />
                             </div>
                             <div className="col-sm">
                                 <SuggestionModal
                                     suggestion={SuggestionStrategy.maybe}
-                                    style={{color: "#FCB70F", borderColor: "#FCB70F", width: 150}}
+                                    style={{ color: "#FCB70F", borderColor: "#FCB70F", width: 150 }}
                                     studentUrl={student._links.self.href}
                                 />
                             </div>
                             <div className="col-sm">
                                 <SuggestionModal
                                     suggestion={SuggestionStrategy.no}
-                                    style={{color: "#F14A3B", borderColor: "#F14A3B", width: 150}}
+                                    style={{ color: "#F14A3B", borderColor: "#F14A3B", width: 150 }}
                                     studentUrl={student._links.self.href}
                                 />
                             </div>
                         </Row>
                     </Col>
                     <Col sm={4}>
-                        <StudentStatus studentUrl={student._links.self.href} status={student.status}/>
+                        <StudentStatus studentUrl={student._links.self.href} status={student.status} />
                     </Col>
                 </Row>
             </footer>
