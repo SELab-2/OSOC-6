b6a5e073e14178bad9afaa64c3440e2ddfff84da
Anne Depuydt <99727145+andpuydt@users.noreply.github.com>
2022-05-22 18:43:40 +0200

    Bugfix/navbar login (#544)
    Navbar when not logged in

--- frontend/src/components/util/navBar.tsx
+++ frontend/src/components/util/navBar.tsx
@@ -4,52 +4,59 @@ import useTranslation from "next-translate/useTranslation";
 import applicationPaths from "../../properties/applicationPaths";
 import { useEditionApplicationPathTransformer } from "../../hooks/utilHooks";
 import OptionsMenu from "./optionsMenu";
-import { useCurrentAdminUser } from "../../hooks/useCurrentUser";
+import { useCurrentAdminUser, useCurrentUser } from "../../hooks/useCurrentUser";
 
 export const NavBar = () => {
     const { t } = useTranslation("common");
     const transformer = useEditionApplicationPathTransformer();
     const currentUserIsAdmin = useCurrentAdminUser();
+    const { user } = useCurrentUser();
 
     return (
         <div className="capitalize" data-testid="nav-bar">
             <Navbar collapseOnSelect sticky="top" expand="lg" bg="dark" variant="dark">
                 <Container>
                     <Navbar.Brand
-                        href={transformer("/" + applicationPaths.assignStudents)}
+                        href={
+                            user
+                                ? transformer("/" + applicationPaths.assignStudents)
+                                : transformer("/" + applicationPaths.login)
+                        }
                         data-testid="navbar-brand"
                     >
                         <Image alt="" src={"/resources/osoc-logo.svg"} width="30" height="30" />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className={"ms-auto"}>
-                            <Nav.Item data-testid="navbar-students">
-                                <Nav.Link href={transformer("/" + applicationPaths.students)}>
-                                    {t("students")}
-                                </Nav.Link>
-                            </Nav.Item>
-                            {currentUserIsAdmin && (
-                                <Nav.Item data-testid="navbar-users">
-                                    <Nav.Link href={transformer("/" + applicationPaths.users)}>
-                                        {t("users")}
+                        {user && (
+                            <Nav className={"ms-auto"}>
+                                <Nav.Item data-testid="navbar-students">
+                                    <Nav.Link href={transformer("/" + applicationPaths.students)}>
+                                        {t("students")}
                                     </Nav.Link>
                                 </Nav.Item>
-                            )}
-                            <Nav.Item data-testid="navbar-projects">
-                                <Nav.Link href={transformer("/" + applicationPaths.projects)}>
-                                    {t("projects")}
-                                </Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item data-testid="navbar-assignstudents">
-                                <Nav.Link href={transformer("/" + applicationPaths.assignStudents)}>
-                                    {t("assign students")}
-                                </Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item data-testid="navbar-profile">
-                                <OptionsMenu />
-                            </Nav.Item>
-                        </Nav>
+                                {currentUserIsAdmin && (
+                                    <Nav.Item data-testid="navbar-users">
+                                        <Nav.Link href={transformer("/" + applicationPaths.users)}>
+                                            {t("users")}
+                                        </Nav.Link>
+                                    </Nav.Item>
+                                )}
+                                <Nav.Item data-testid="navbar-projects">
+                                    <Nav.Link href={transformer("/" + applicationPaths.projects)}>
+                                        {t("projects")}
+                                    </Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item data-testid="navbar-assignstudents">
+                                    <Nav.Link href={transformer("/" + applicationPaths.assignStudents)}>
+                                        {t("assign students")}
+                                    </Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item data-testid="navbar-profile">
+                                    <OptionsMenu />
+                                </Nav.Item>
+                            </Nav>
+                        )}
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
