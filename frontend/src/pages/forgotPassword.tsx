b6180b5b0ec2166bda1514f070cd17b9a3834d4a
Kasper Demeyere <kasperdemeyere@hotmail.com>
2022-05-22 17:04:35 +0200

    add link back to login

--- frontend/src/pages/forgotPassword.tsx
+++ frontend/src/pages/forgotPassword.tsx
@@ -6,6 +6,7 @@ import { capitalize } from "../utility/stringUtil";
 import { postForgotPasswordEmail } from "../api/calls/userCalls";
 import { Background } from "../components/util/background";
 import styles from "../styles/resetComponent.module.css";
+import applicationPaths from "../properties/applicationPaths";
 
 /**
  * The forgot password page, where a user can enter their email to request a password reset.
@@ -21,6 +22,11 @@ const ForgotPassword: NextPage = () => {
                 <div className={styles.reset_box}>
                     <h2>{capitalize(t("forgot password title"))}</h2>
                     <ForgotComponent handler={(email: string) => postForgotPasswordEmail(email)} />
+                    <div className="mt-3">
+                        <a href={"/" + applicationPaths.login} className={styles.link}>
+                            {capitalize(t("back to login"))}
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
