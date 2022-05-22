ecf13bf5ed03433d1a450d0613b22d4acb3dce62
andpuydt <anne.depuydt@ugent.be>
2022-05-22 13:10:29 +0200

    Requested changes

--- data-injector/src/api/calls/studentCalls.ts
+++ data-injector/src/api/calls/studentCalls.ts
@@ -6,7 +6,7 @@ import {
     getEntityOnUrl,
     getQueryUrlFromParams,
 } from "./baseCalls";
-import { IStudent, OscExperience, Status, studentCollectionName } from "../entities/StudentEntity";
+import { IStudent, OsocExperience, Status, studentCollectionName } from "../entities/StudentEntity";
 import { AxiosResponse } from "axios";
 
 export interface IStudentQueryParams {
@@ -36,10 +36,10 @@ export function getStudentOnUrl(url: string): Promise<IStudent> {
 export function constructStudentQueryUrl(url: string, params: IStudentQueryParams): string {
     const experience: string[] = [];
     if (params.studentCoach) {
-        experience.push(OscExperience.yes_studentCoach);
+        experience.push(OsocExperience.yes_studentCoach);
     }
     if (params.alumni) {
-        experience.push(OscExperience.yes_studentCoach, OscExperience.yes_noStudentCoach);
+        experience.push(OsocExperience.yes_studentCoach, OsocExperience.yes_noStudentCoach);
     }
 
     const queryParams: { [k: string]: any } = {};
